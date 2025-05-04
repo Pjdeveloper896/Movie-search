3cc4381cbc85322a878cb547f6242e45f415db51
Prasoon <prasoonjadon.pis6@gmail.com>
2025-05-04 10:27:47 +0530

    Create header.js

---
+++ header.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+function Header() {
+  
+  return (
+    
+  <div>
+  <h1 className="heading title">
+    movie search 
+  </h1>
+  </div>
+  );
+}
+
+export default Header;
